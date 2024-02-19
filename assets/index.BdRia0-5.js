5f2bcdb3e4cfdf7cefe11c7974c7aa562012f282
DanRotaru <dan.rotaru@lislex.com>
2024-02-19 17:00:09 +0200

    Updated router and added nojekyll file

---
+++ assets/index.BdRia0-5.js
@@ -0,0 +1,1 @@
+import{_ as e,v as c,t as n}from"./entry.B3i1fN-A.js";const t={};function o(r,s){return n(),c("div",null," Home View ")}const a=e(t,[["render",o]]);export{a as default};
--- assets/test.CvuqYJRO.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,v as t,t as c}from"./entry.oGkzQFnA.js";const n={};function r(s,o){return c(),t("div",null," Test View ")}const a=e(n,[["render",r]]);export{a as default};
